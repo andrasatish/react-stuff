b97e55f7e415c8a09ad9b2d42bc3a56d7c51f35f
Satish <43448483+andrasatish@users.noreply.github.com>
2024-08-30 17:53:59 +0530

    curd operation

--- src/components/03.curd/curd.jsx
+++ src/components/03.curd/curd.jsx
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './curd.css';
-import { Button, Space, Form, Input, InputNumber } from 'antd';
+import { Button, Space, Form, Input, InputNumber, Select, Radio, Checkbox } from 'antd';
 
 const Curd = () => {
+    const [form] = Form.useForm();
+    const statesList = [{ label: 'Telangana', value: 'TL' }, { label: 'Andhra Pradesh', value: 'AP' }, { label: 'Delhi', value: 'Delhi' }];
+    const genderOptions = [{ label: 'Male', value: 'male' }, { label: 'Female', value: 'female' }];
+    const placeTypeList = [{ label: 'Public Visit', value: 'public' }, { label: 'Priavate Visit', value: 'private' }];
+
+    const [genderData, setGender] = useState('male');
+    const [touristList, setTouristList] = useState([]);
 
     const onFinish = (values) => {
-        console.log('Success:', values);
+        setTouristList([values,...touristList]);
+        form.resetFields();
     };
 
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
     };
 
+    const onGenderChange = ({ target: { value } }) => {
+        setGender(value)
+    }
+
     return (
         <div className='curd-container'>
             <div className='curd-item-container'>
-                <h2>Form</h2>
+                <h2>Fill the tour form</h2>
                 <Form
                     name="basic"
                     labelCol={{ span: 6 }}
@@ -27,6 +39,9 @@ const Curd = () => {
                     autoComplete="off"
                     colon={false}
                     labelAlign={'left'}
+                    size={'small'}
+                    form={form}
+                    
                 >
                     <Form.Item
                         label="Name"
@@ -52,21 +67,43 @@ const Curd = () => {
                         <InputNumber placeholder='Age' min={1} max={100} style={{ width: '100%' }} />
                     </Form.Item>
 
-
                     <Form.Item
                         label="State"
                         name="state"
-                        rules={[{ required: true, message: 'Please input state' }]}
+                        rules={[{ required: true, message: 'Please select state' }]}
                     >
-                        <Input placeholder='State' />
+                        <Select
+                            showSearch
+                            placeholder="Select State"
+                            optionFilterProp="label"
+                            options={statesList}
+                        />
                     </Form.Item>
+
+
+                    <Form.Item
+                        label="Gender"
+                        name="gender">
+                        <Radio.Group options={genderOptions} onChange={onGenderChange} value={genderData} />
+                    </Form.Item>
+
+                    <Form.Item
+                        label={
+                            <span style={{ textWrap: 'wrap', height: '70px' }}>Select Place Type to Visit</span>
+                        }
+                        name="placeType"
+                    >
+                        <Checkbox.Group options={placeTypeList} />
+                    </Form.Item>
+
+
 
                     <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
                         <Space size={"small"}>
                             <Button type="primary" htmlType="submit">
                                 Submit
                             </Button>
-                            <Button type="primary">
+                            <Button type="primary" htmlType="reset">
                                 Reset
                             </Button>
                         </Space>
@@ -80,24 +117,42 @@ const Curd = () => {
                         <tr>
                             <th>Name</th>
                             <th>Location</th>
-                            <th>State</th>
                             <th>Age</th>
+                            <th>State</th>
+                            <th>Gender</th>
+                            <th>Place Type to Visit</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>Sudheer</td>
-                            <td>Hyderabad</td>
-                            <td>Telangana</td>
-                            <td>28</td>
-                            <td>
-                                <Space size={"small"}>
-                                    <Button type="primary" size={"small"}>Edit</Button>
-                                    <Button type="primary" size={"small"}>Delete</Button>
-                                </Space>
-                            </td>
-                        </tr>
+                        {
+                            touristList.map((tourist) => {
+                                return (
+                                    <tr>
+                                        <td>{tourist.name}</td>
+                                        <td>{tourist.location}</td>
+                                        <td>{tourist.age}</td>
+                                        <td>{tourist.state}</td>
+                                        <td>{tourist.gender}</td>
+                                        <td>
+                                            {
+                                                tourist.placeType.map((place)=>{
+                                                    return (
+                                                        <span className={'pills'}>{place}</span>
+                                                    )
+                                                })
+                                            }
+                                            </td>
+                                        <td>
+                                            <Space size={"small"}>
+                                                <Button type="primary" size={"small"}>Edit</Button>
+                                                <Button type="primary" size={"small"}>Delete</Button>
+                                            </Space>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </table>
             </div>
